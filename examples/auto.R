042ab61c3936e2cd83b1122a21e7b621c860861d
Joshua Bloom <jbloom@mednet.ucla.edu>
2021-09-29 21:11:42 -0700

    move local mirror to wsl2 filesystem

--- examples/auto.R
+++ examples/auto.R
@@ -3,8 +3,9 @@ basedir.dir='/mnt/e/swabseq2/'
 # location of shared drive
 remote.dir=paste0(basedir.dir, 'remote/')
 # location of mirror 
-localmirror.dir=paste0(basedir.dir, 'localmirror/')
-    
+#localmirror.dir=paste0(basedir.dir, 'localmirror/')
+#move to wsl2 filesystem for better performance    
+localmirror.dir='/home/swabseq/swabseq2/localmirror/'
 #bcl.dir is path to location to save bcls (keep off shared drive), 
 #can download bcls manually here and script will auto-recognize them
 bcl.dir=paste0(basedir.dir, 'bcls/')
